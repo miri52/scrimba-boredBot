beb5a3ad805563201f6252c1d32b2875201bb765
Miriama Svítková <miri52@users.noreply.github.com>
2021-07-13 11:58:22 +0100

    Update the dog img
    - change the dog img from bored to happy when an activity is found

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const btn = document.querySelector('.btn')
-const activityDOM = document.getElementById('activity')
-
+const btn = document.querySelector(".btn");
+const activityDOM = document.getElementById("activity");
+const dogImg = document.querySelector(".dog-img");
 
 const findActivity = () => {
-    const url = 'https://apis.scrimba.com/bored/api/activity'
-    fetch(url).then(res => res.json()).then(data => {
-        activityDOM.textContent = data.activity;
-        activityDOM.classList.add('activity')
+  const url = "https://apis.scrimba.com/bored/api/activity";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      activityDOM.textContent = data.activity;
+      activityDOM.classList.add("activity");
+      dogImg.src = "happy-dog.jpg";
+      dogImg.alt = "black pug in a red beret hat";
+    });
+};
 
-    })
-}
-
-btn.addEventListener('click', findActivity)
+btn.addEventListener("click", findActivity);
